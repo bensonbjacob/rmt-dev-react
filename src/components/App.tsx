c17eaf6b65009d83a17187d3f1a7c2d7befd63f9
Jacob <jacob.benson.dev@gmail.com>
2023-12-31 10:13:06 -0600

    Refactor job item fetch into custom hook
    Centralized job item fetching logic by introducing a new custom hook, `useJobItem`, which replaced the direct `fetch` call within the `App` component. This aligns with best practices for managing data fetching and state in React, allowing for cleaner component code and improved reusability. Additionally, redundant `useEffect` import in `App` has been removed, streamlining the import statements.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Background from './Background';
 import Container from './Container';
 import Footer from './Footer';
@@ -12,19 +12,13 @@ import ResultsCount from './ResultsCount';
 import JobList from './JobList';
 import PaginationControls from './PaginationControls';
 import SortingControls from './SortingControls';
-import { useActiveId, useJobItems } from '../lib/hooks';
-import { BASE_API_URL } from '../lib/constants';
+import { useActiveId, useJobItem, useJobItems } from '../lib/hooks';
 
 function App() {
   const [searchText, setSearchText] = useState('');
   const [jobItems, isLoading] = useJobItems(searchText);
   const activeId = useActiveId();
-
-  useEffect(() => {
-    if (!activeId) return;
-
-    fetch(`${BASE_API_URL}/${activeId}`);
-  }, [activeId]);
+  const jobItem = useJobItem(activeId);
 
   return (
     <>
