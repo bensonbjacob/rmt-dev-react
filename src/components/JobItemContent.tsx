5cf257659cb7971d0a42aa69bdcef9975dc5e84f
Jacob <jacob.benson.dev@gmail.com>
2023-12-31 11:55:42 -0600

    Add loading state and visual feedback for job data fetch
    Updated `useJobItem` hook to track the loading state when fetching job details. Introduced a `LoadingJobContent` component that displays a spinner until the data is loaded, improving user experience by providing immediate feedback during data retrieval. This change enables users to recognize that content is being loaded in the background, preventing confusion over empty content areas and improving the responsiveness of the application's interface.

--- src/components/JobItemContent.tsx
+++ src/components/JobItemContent.tsx
@@ -1,9 +1,14 @@
 import { useActiveId, useJobItem } from '../lib/hooks';
 import BookmarkIcon from './BookmarkIcon';
+import Spinner from './Spinner';
 
 export default function JobItemContent() {
   const activeId = useActiveId();
-  const jobItem = useJobItem(activeId);
+  const [jobItem, isLoading] = useJobItem(activeId);
+
+  if (isLoading) {
+    return <LoadingJobContent />;
+  }
 
   if (!jobItem) {
     return <EmptyJobContent />;
@@ -97,6 +102,16 @@ export default function JobItemContent() {
             appreciate it!
           </p>
         </footer>
+      </div>
+    </section>
+  );
+}
+
+function LoadingJobContent() {
+  return (
+    <section className='job-details'>
+      <div>
+        <Spinner />
       </div>
     </section>
   );
