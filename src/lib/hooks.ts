6c71d13bcd5f33f9ed53365b051c7ddd389a632a
Jacob <jacob.benson.dev@gmail.com>
2023-12-30 18:53:05 -0600

    Limit job items to first 7 entries
    Optimized the job listings to improve UI performance and clarity by slicing the job items array to only include the first 7 entries. This change impacts the App component where job listings are rendered as well as the useJobItems hook to ensure consistency in data handling across components.

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -4,6 +4,8 @@ export function useJobItems(searchText: string) {
   const [jobItems, setJobItems] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
+  const jobItemsSliced = jobItems.slice(0, 7);
+
   useEffect(() => {
     if (!searchText) return;
     const fetchData = async () => {
@@ -19,5 +21,5 @@ export function useJobItems(searchText: string) {
     fetchData();
   }, [searchText]);
 
-  return { jobItems, isLoading };
+  return { jobItemsSliced, isLoading };
 }
